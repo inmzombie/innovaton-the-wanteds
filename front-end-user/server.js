7f2a25161e061af72c4fa6c59f82c4feba0cd29b
Ian Franco Sebástian <ian.pozo.m@gmail.com>
2018-11-24 12:03:01 -0300

    Reorganización

--- front-end-user/server.js
+++ front-end-user/server.js
@@ -4,15 +4,21 @@
 
 var express = require('express');
 
-var cors = require('express-cors')
-
 var app = express();
 
 var server = require('http').createServer(app);
 
-var io = require('socket.io')(server, { origins: '*:*'});
+var io = require('socket.io')(server, {
+    origins: '*:*'
+});
 
 app.use(express.static(__dirname + '/www'));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 
@@ -23,10 +29,10 @@ io.on('connection', function (socket) {
 /**
  * ruta para setear valores de pulso cardíaco
  */
-app.get('/setvalue/:value', function(req, res) { 
+app.get('/setvalue/:value', function (req, res) {
     console.log(req.params.value);
     io.emit('update', req.params.value);
-    return res.status(200).json(req.params.value) 
+    return res.status(200).json(req.params.value)
 });
 
 
