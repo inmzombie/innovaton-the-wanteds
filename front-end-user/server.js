0cc591826650efdfbc0d363943b2fcd68f404308
Diego Barros <diego.barros@globant.com>
2018-11-24 06:24:07 -0300

    socket was added

--- front-end-user/server.js
+++ front-end-user/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static(__dirname + '/www'));
 app.use(express.static(__dirname + '/node_modules'));
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('conection', function() {});
+server.listen(3000);
 
-app.listen(3000, function () {
+/*app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-});
+}); */
