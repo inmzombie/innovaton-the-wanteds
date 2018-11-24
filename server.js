6be5a9c9abd36a0ee0807116fd3ac1b44bfe8aed
Ian Franco Sebástian <ian.pozo.m@gmail.com>
2018-11-24 19:15:51 -0300

    Estructura de poyecto

--- server.js
+++ server.js
@@ -2,45 +2,76 @@
  * front end user server
  */
 
-var express = require('express');
+import https from 'https'
+import express from 'express'
+import io from 'socket.io'
+import fs from 'fs'
 
-var app = express();
+let app = express();
 
-var server = require('http').createServer(app);
-
-var io = require('socket.io')(server, {
-    origins: '*:*'
-});
-
-app.use(express.static(__dirname + '/www'));
-
+/**
+ * HANDHELD KNIFE BATTLE CORS
+ */
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
-app.use('/scripts', express.static(__dirname + '/node_modules'));
+// Loads certificate
+const httpsOptions = {
+	key: fs.readFileSync('./docker/nginx/key.pem'),
+	cert: fs.readFileSync('./docker/nginx/cert.pem')
+}
 
-io.on('connection', function (socket) {
-    console.log('---> Socket conectado');
+// Server instance
+let server = https.createServer(httpsOptions, app);
+
+/**
+ * SOCKETS.IO INTEGRATION
+ */
+io(server, {
+	origins: '*:*'
 });
 
 /**
- * ruta para setear valores de pulso cardíaco
+ * STATIC FILES - FRONT END
+ */
+app.use(express.static(__dirname + '/www'));
+
+
+/**
+ * ROUTES SCRIPTS CALLS TO node_modules
+ */
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+
+/**
+ * SOCKETS CONFIGURATION
+ */
+
+io().on('connection', function (socket) {
+	console.log('---> Socket conectado');
+});
+
+/**
+ * ROUTES
+ */
+app.get('/', (req, res) => {
+	res.send('Monit-ON v1')
+})
+
+/**
+ * @description :: actualiza el valor de HR en front.
  */
 app.get('/setvalue/:value', function (req, res) {
-    console.log(req.params.value);
-    io.emit('update', req.params.value);
-    return res.status(200).json(req.params.value)
+	console.log(req.params.value);
+	io.emit('update', req.params.value);
+	return res.status(200).json(req.params.value)
 });
 
 
 let serverport = 3000
 server.listen(serverport, function () {
-    console.log(`
-    ###########################################
-    #        API RUNNING AT ${serverport}     #
-    ###########################################
-    `);
+	console.log(`[SUCCESS] API RUNNING AT PORT ${serverport}
+	`);
 });
