442e26a2bf6a8990d3c1b59c55362514f196bee8
Ian Franco Sebástian <ian.pozo.m@gmail.com>
2018-11-24 10:52:34 -0300

    Orden

--- front-end-user/server.js
+++ front-end-user/server.js
@@ -1,25 +1,40 @@
+/**
+ * front end user server
+ */
+
 var express = require('express');
+
 var cors = require('express-cors')
 
 var app = express();
-app.use(express.static(__dirname + '/www'));
-app.use(express.static(__dirname + '/node_modules'));
 
 var server = require('http').createServer(app);
+
 var io = require('socket.io')(server, { origins: '*:*'});
+
+app.use(express.static(__dirname + '/www'));
+
+app.use('/scripts', express.static(__dirname + '/node_modules'));
 
 io.on('connection', function (socket) {
     console.log('a user connected');
 });
 
+/**
+ * ruta para setear valores de pulso cardíaco
+ */
 app.get('/setvalue/:value', function(req, res) { 
     console.log(req.params.value);
     io.emit('update', req.params.value);
     return res.status(200).json(req.params.value) 
 });
 
-server.listen(3000);
 
-/*app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-}); */
+let serverport = 3000
+server.listen(serverport, function () {
+    console.log(`
+    ###########################################
+    #        API RUNNING AT ${serverport}     #
+    ###########################################
+    `);
+});
