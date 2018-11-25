615f4c2807d4d59ac45f58da06fc7e7d1f47f718
Ian Franco Sebástian <ian.pozo.m@gmail.com>
2018-11-25 09:00:28 -0300

    automatización de elementos de repositorio

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # CORRER APLICACION
 
+## Corre aplicación en puerto 3000 por defecto:
+
 `npm run dev`
 
-> Corre aplicación en puerto 3000 por defecto
+## Genera bundle de módulos JS:
 
-  
+`npm run build`
+
+`npm run buid:watch`
+
+# FAQ
+
+- Agregar según corresponda
---
+++ src/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import MiBand from './miband'
+
+import jQuery from 'jquery'
+
+const bluetooth = navigator.bluetooth;
+
+const output = document.querySelector('#output');
+
+function updateHeartRadio(data) {
+	jQuery('.heart-ration').html(data);
+}
+
+function updateCalories(data) {
+	jQuery('.cal').html(data)
+}
+
+function updateMov(data) {
+	jQuery('.mov').html(data)
+}
+
+function log() {
+	document.querySelector('main').style.display = 'block';
+
+	output.innerHTML += [...arguments].join(' ') + '\n';
+}
+
+async function scan() {
+	if (!bluetooth) {
+		log('WebBluetooth is not supported by your browser!');
+		return;
+	}
+
+	try {
+		log('Requesting Bluetooth Device...');
+		const device = await bluetooth.requestDevice({
+			filters: [{
+				services: [MiBand.advertisementService]
+			}],
+			optionalServices: MiBand.optionalServices
+		});
+
+		device.addEventListener('gattserverdisconnected', () => {
+			log('Device disconnected');
+		});
+
+		await device.gatt.disconnect();
+
+		log('Connecting to the device...');
+		const server = await device.gatt.connect();
+		log('Connected');
+
+		let miband = new MiBand(server);
+
+		await miband.init();
+
+		//await test_all(miband, log);
+
+	} catch (error) {
+		log('Argh!', error);
+	}
+}
+
+document.querySelector('#bt_connect').addEventListener('click', scan)
